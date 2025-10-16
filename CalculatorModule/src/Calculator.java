f5f6148884618467ba5e76da7e314d26da93416a
Igor Grabler <igor.grabler@gmail.com>
2025-10-16 13:27:16 +0200

    Test

--- CalculatorModule/src/Calculator.java
+++ CalculatorModule/src/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
         while (true) {
 
             System.out.println("----Calculator----");
-            System.out.println("1) Addition");
+            System.out.println("1) Add!tion");
             System.out.println("2) Subtraction");
             System.out.println("3) Multiplication");
             System.out.println("4) Division");
